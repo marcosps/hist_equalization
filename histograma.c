65ee98ae72e63e79c8fe5ab89412ff6985f73286
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2012-06-19 23:09:29 -0300

    Escolher imagem a ser equalizada na chamada do programa
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>

--- histograma.c
+++ histograma.c
@@ -44,9 +44,9 @@ void cdf(int *ori, int *nova, int maior_pixel)
 }
 
 /* Gera nova imagem PGM já equalizada */
-void monta_novo_arquivo(int *cdf)
+void monta_novo_arquivo(char *ori_file, int *cdf)
 {
-	FILE *f = fopen("Unequalized_Hawkes_Bay.pgm","r");
+	FILE *f = fopen(ori_file,"r");
 	FILE *n = fopen("nova.pgm", "w");
 	int linha = 1, num_pos = 0;
 	char *line = NULL;
@@ -95,17 +95,23 @@ void monta_novo_arquivo(int *cdf)
 	free(line);
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
+
+	if (argc < 2) {
+		printf("Uso do programa: ./h <caminho_para_imagem>.pgm\n");
+		return 1;
+	}
+
 	/*É somando 1 para que não seja necessário utilizar o posição 0.
 	 *Dessa forma o número de ocorrências de pixels com valor 1 fica
          *na posição 1 e assim em diante */
 	int *ocorrenciasPixels, *acumulada;
 	char numero[TAMANHO_DIGITO_NUMERO];
-	FILE *arquivo = fopen("Unequalized_Hawkes_Bay.pgm","r");
+	FILE *arquivo = fopen(argv[1],"r");
 
 	if(!arquivo){
-		printf("Perdeu playboy! Arquivo não encontradoo.\n");
+		printf("Perdeu playboy! Arquivo não encontrado.\n");
 		return 1;
 	}
 	
@@ -207,7 +213,7 @@ int main(void)
 	fclose(arquivo);
 	
 	printf("Gerando nova imagem PGM...\n");
-	monta_novo_arquivo(acumulada);
+	monta_novo_arquivo(argv[1], acumulada);
 	printf("Nova imagem gerada com sucesso!\n");
 
 	free(ocorrenciasPixels);
